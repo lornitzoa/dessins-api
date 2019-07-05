b586d092bcef466f0a38694e1ea525235dd5db17
Anna Lornitzo <lornitzoa@gmail.com>
2019-07-05 12:25:24 -0400

    testing heroku database config in dessin.rb

--- app/models/dessin.rb
+++ app/models/dessin.rb
@@ -1,7 +1,7 @@
 class Dessin
   if(ENV['DATABASE_URL'])
-    uri = UIR.parse(ENV['DATABASE_URL'])
-    DB = PG.connect(uri.hostmame, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    uri = URI.parse(ENV['DATABASE_URL'])
+    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
   else
     DB = PG.connect({:host => "localhost", :port => 5432, :dbname => "d3ssins-backend_development"})
   end
